d77b25b46f1cba2d09e5c413093036f800307da4
jarrade <jarradepierce@yahoo.com>
2017-02-21 20:24:23 -0800

    initial commit of iteration.rb

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -6,6 +6,8 @@ def message
 end
 message {puts "another blah"}
 
+
+
 #Release 1
 animals = ["Zebra", "Lion", "Monkey", "Harambe"]
 
@@ -34,8 +36,54 @@ animals.map! do |zoo_animal|
 end
 puts animals
 
+puts " "
+puts " "
+
+#Release 2
+=begin
+animals.delete_if{ 
+	|animal| animal.length > 5
+}
+puts animals
+
+puts " " 
+
+animals.keep_if{
+	|animal| animal.length > 4
+}
+puts animals
+
+if animals.length < 2
+	animals.each do
+	 animals.replace(["Zebra", "Wolf", "Bear", "Tiger", "Girraffe"])
+	end
+	puts animals
+end
+=end
+
+puts ""
+puts "HASHES!"
+puts ""
+oceans.delete_if{
+	|fish, ocean| fish >= "s"
+}
+puts oceans
+
+puts ""
+puts "keep_if"
+puts ""
+oceans.keep_if{
+	|fish, ocean| ocean == "Arctic"
+}
+puts oceans
 
 
+oceans.each do |fish, ocean|
+	if fish.length > 3
+		puts fish
+	end
+
+end
 
 
 
