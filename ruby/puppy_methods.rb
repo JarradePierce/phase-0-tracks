3cc139e9c02b70859a7e5e0332c80dc16aab2d6e
Jarrade Pierce <slim7@Jarrades-MacBook-Air.local>
2017-03-03 19:47:38 -0800

    finished release 1 and 2

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,5 +1,9 @@
 class Puppy
 
+	def initialize
+		puts "Initializing new Puppy instance"
+	end
+
   def fetch(toy)
     puts "I brought back the #{toy}!"
     toy
@@ -25,10 +29,61 @@ class Puppy
   end
 
 end
+
+
 puppy = Puppy.new
 puppy.fetch("Bone")
 puppy.speak(5)
 puppy.roll_over
 puts puppy.dog_years(3)
 puppy.shake("jays")
+
+
+class Candy
+
+	def initialize
+		puts "Initializing candy instance..."
+	end
+
+	def pick_type_of_candy(candy_type)
+		puts "I choose #{candy_type}!"
+	end
+
+	def eats_candy(candy)
+		puts "I am now eating #{candy}!"
+	end
+
+end
+
+#candy = Candy.new
+#candy.pick_type_of_candy("Reeses")
+#candy.eats_candy("Reeses")
+
+candies = []
+50.times do
+	candies << Candy.new
+end
+
+candies.each do |methods|
+	methods.pick_type_of_candy("Reeses")
+	methods.eats_candy("Reeses")
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
