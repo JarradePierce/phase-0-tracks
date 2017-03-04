2ae9edfe6b5f47a05c037a6b8392201f445399fa
Jarrade Pierce <slim7@Jarrades-MacBook-Air.local>
2017-03-03 19:01:52 -0800

    finished realse 0

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,30 @@ class Puppy
     toy
   end
 
+  def speak(number)
+  	number.times do |woof|
+  		puts "Woof!"
+  	end
+  end
+
+  def roll_over
+  	puts "*Roll over*"
+  end
+
+  def dog_years(human_years)
+  	dog_year = human_years * 7
+  	dog_year
+  end
+
+  def shake(name)
+  	puts "Shakes #{name}'s hand"
+  end
+
 end
+puppy = Puppy.new
+puppy.fetch("Bone")
+puppy.speak(5)
+puppy.roll_over
+puts puppy.dog_years(3)
+puppy.shake("jays")
 
