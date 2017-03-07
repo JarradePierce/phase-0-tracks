abdb7423a6e49e14f7984f9bc27d3854fda2da54
Jarrade Pierce <slim7@Jarrades-MacBook-Air.local>
2017-03-07 13:19:22 -0800

    finished gps week 6

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -37,13 +37,13 @@ class VirusPredictor
   end
 
   def virus_effects
-     predicted_deaths(@population_density, @population, @state)
-     speed_of_spread(@population_density, @state)
+     predicted_deaths
+     speed_of_spread
   end
 
   private
 
-  def predicted_deaths(population_density, population, state)
+  def predicted_deaths
     # predicted deaths is solely based on population density
     if @population_density >= 200
       number_of_deaths = (@population * 0.4).floor
@@ -61,7 +61,7 @@ class VirusPredictor
 
   end
 
-  def speed_of_spread(population_density, state) #in months
+  def speed_of_spread #in months
     # We are still perfecting our formula here. The speed is also affected
     # by additional factors we haven't added into this functionality.
     speed = 0.0
@@ -91,7 +91,7 @@ end
 
 
 alabama = VirusPredictor.new("Alabama", STATE_DATA["Alabama"][:population_density], STATE_DATA["Alabama"][:population])
-
+alabama.virus_effects
 jersey = VirusPredictor.new("New Jersey", STATE_DATA["New Jersey"][:population_density], STATE_DATA["New Jersey"][:population])
 jersey.virus_effects
 
