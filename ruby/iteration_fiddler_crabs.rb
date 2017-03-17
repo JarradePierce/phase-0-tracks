dcb005a2d4f9b8b09481666dba6b9103ce62d3b6
Jarrade Pierce <slim7@Jarrades-MacBook-Air.local>
2017-03-16 23:01:12 -0700

    finished 5.3 fiddler crabs

--- ruby/iteration_fiddler_crabs.rb
+++ ruby/iteration_fiddler_crabs.rb
@@ -1,20 +1,14 @@
 
 cars = {
-	make: "Tesla",
-	model: "s",
-	year: 2017,
-
-	make: "Nissan",
-	model: "240 sx",
-	year: 1989,
-
-	make: "Ford",
-	model: "Mustang",
-	year: 2016
+	tesla:  "model s",
+	ford: "mustang",
+	chevy: "camero",
+	nissan: "240 sx"
 }
 
 
-drivers = ["Gordon", "Patrick", "Harvick"]
+drivers = ["Gordon", "Patrick", "Harvick", "Hamlin"]
+
 =begin
 cars.each do |key, value| 
 	puts "#{key}: #{value}"
@@ -23,18 +17,83 @@ end
 drivers.each do |driver|
 	puts driver
 end
-=end
 
 drivers.map do |driver|
 	driver.next
 end
+=end
 
 =begin
 drivers.map! do |driver|
 	driver.next
 end
 =end 
-puts drivers
+#puts drivers
+
+#Deleting Drivers greater than 6
+#drivers.delete_if{ 
+#	|driver| driver.length > 6
+#}
+
+#KEEPING Driveres greater than 6
+#drivers.keep_if{
+#	|driver| driver.length > 6
+#}
+
+#Data Structure
+#drivers.reverse_each{
+#	|driver| driver > 1 
+	#puts driver
+#}
+
+
+# REMOVES 
+#drivers.keep_if{
+#	|driver| driver.length < 7
+ #}
+
+#puts drivers
+
+#Deletes if value is greater then 7
+#cars.delete_if{
+#	|key, value| value.length >= 7
+#}
+
+#Satisfies condition is value greater or equal to 7
+#cars.keep_if{
+#	|key, value| value.length >= 7
+#}
+
+#Iterates through hash
+cars.each_key{
+	|key, value| puts key
+ }
+
+#Iterates through condition and gives boolean
+#cars.each_value{
+#	|value| short_names = value.length > 6
+#	puts short_names
+#}
+
+cars.each do |key, value|
+	puts "#{key}: #{value}"
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
