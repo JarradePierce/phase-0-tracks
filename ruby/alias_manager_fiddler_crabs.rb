9114da5c483df262fdbefbc7827fcb55bcd2bf1e
Jarrade Pierce <slim7@Jarrades-MacBook-Air.local>
2017-03-17 15:33:59 -0700

    finished 5.5 solo challenge fiddler crabs

--- ruby/alias_manager_fiddler_crabs.rb
+++ ruby/alias_manager_fiddler_crabs.rb
@@ -9,32 +9,72 @@
 #alphabet. So 'a' would become 'e', 'u' would become 'a', and 'd' would become 'f'.
 
 
-def spy_name(name)
-	spy_full_name = name
+def spy_name
+#check if user is done creating names
+done = false
+#array that holds encrypted names
+encrypted_names = []
 
-	
-	split_word = spy_full_name.split('')
-	secret_name = []
 
-	split_word.each do |letter|
-		if letter =='a'
-			letter = 'e'
+	#loops until done equals to true
+	while done == false
+		#temporary user name array
+		user_name = []
+		#new name is user_name join as a string
+		new_name = user_name.join('')
+		puts new_name
 
-		elsif letter == 'e'
-			letter = 'i'
+		puts "Enter a name"
+		name = gets.chomp
 
-		elsif letter == 'i'
-			letter = 'o'
-
-		elsif letter == 'o'
-			letter = 'u'
-
-		elsif letter == 'u'
-			letter = 'a'
+		#check if user is done with adding names
+		if name == 'quit'
+			puts encrypted_names
+			done = true
 		end
-		puts letter
+
+		#spliting the user name and reversing it then spliting it into an array
+		secret_name = name.split(' ')
+		reverse_name = secret_name.reverse
+		join_name = reverse_name.join('')
+
+		split_name = join_name.split('')
+
+			#iterating through split_name changing letters if equal to 'aeiou'
+			split_name.each do |letter|
+				if letter =='a'
+					letter = 'e'
+
+				elsif letter == 'e'
+					letter = 'i'
+
+				elsif letter == 'i'
+					letter = 'o'
+
+				elsif letter == 'o'
+					letter = 'u'
+
+				elsif letter == 'u'
+					letter = 'a'
+				end
+				#pushing letter to temporary user_name array
+				user_name.push(letter)
+			end
+			#creating a new name using user_name
+			new_name = user_name.join('')
+			#pushing new_name to encrypted_names
+			encrypted_names.push(new_name)
 	end
 end
 
+spy_name
 
-spy_name("jay pierce")
+
+
+
+
+
+
+
+
+
