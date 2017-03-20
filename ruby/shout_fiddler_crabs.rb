b4bef4df8106460afda2224cd3411d0efdf93f87
Jarrade Pierce <slim7@Jarrades-MacBook-Air.local>
2017-03-19 20:21:50 -0700

    finished release 2

--- ruby/shout_fiddler_crabs.rb
+++ ruby/shout_fiddler_crabs.rb
@@ -1,9 +1,36 @@
 #1. Declare your module. The syntax for declaring a module is pretty simple:
+module Flight
+  def take_off(altitude)
+    puts "Taking off and ascending until reaching #{altitude} ..."
+  end
+end
+
 module Shout
   def self.yell_angrily(words)
     words + "!!!" + " :("
   end
+
+  def self.yell_happily(words)
+  	words + "!!!" + " :)"
+  end
+
 end
-#2. In your terminal, run IRB from the directory that contains shout.rb. We can
-#use require_relative to load our new module:
+
+#declare classes for a bird and a plane:
+
+class Bird
+	include Flight
+end
+
+class Plane
+	include Flight
+end
+
+bird = Bird.new
+bird.take_off(800)
+
+plane = Plane.new
+plane.take_off(30000)
+
+
 
