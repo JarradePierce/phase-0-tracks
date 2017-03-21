ce69eb087a5ad7332190e0ec49a8122295a80bdb
Jarrade Pierce <slim7@Jarrades-MacBook-Air.local>
2017-03-20 19:32:15 -0700

    finished 6.2

--- ruby/puppy_methods_fiddler_crabs.rb
+++ ruby/puppy_methods_fiddler_crabs.rb
@@ -1,5 +1,9 @@
 class Puppy
 
+	def initialize
+		puts "Initializing..."	
+	end
+
 	def fetch(toy)
     	puts "I brought back the #{toy}!"
   		toy
@@ -26,12 +30,54 @@ class Puppy
 	end
 end
 
-puppy = Puppy.new
-puppy.fetch("ball")
-puppy.speak(5)
-puppy.roll_over
-puppy.dog_years(2)
-puppy.shake
+#puppy = Puppy.new
+#puppy.fetch("ball")
+#puppy.speak(5)
+#puppy.roll_over
+#puppy.dog_years(2)
+#puppy.shake
+
+
+class Football
+
+	def initialize
+		puts "Initializing..."
+	end
+
+	def pass(name, number)
+		puts "Pass thrown to #{name} number #{number}"
+	end
+
+	def run(name, number)
+		puts "Run by #{name} number #{number}"
+	end
+end
+
+football = Football.new
+football_arr = []
+
+50.times do
+	football_arr << football
+end 
+
+football_arr.each do |index|
+	football.run("Zeak", 21)
+	football.pass('Rogers', 12)
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
