8eb7fae9d4a11b03af82913c231b2717e759be9f
Jarrade Pierce <slim7@Jarrades-MacBook-Air.local>
2017-04-13 16:25:54 -0700

    finished 9.3 jquery

---
+++ web_dev/jquery/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+
+	$("#sign-up-html").hide();
+	$("#sign-up-css").hide();
+
+
+    $("#header").click(function(){
+        $(this).hide();
+    });
+
+    $("#html-button").click(function(){
+    	$("#sign-up-html").show();
+    });
+
+    $("#css-button").click(function(){
+    	$("#sign-up-css").show();
+    });
+});
